3ac9d8a3031d27044e76f9f3c0cded918d5085ee
aadityavishnoi <vishnoiaaditya29@gmail.com>
2025-08-31 16:14:13 +0530

    commit

---
+++ arraylist.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << n << endl;
+    for (int i = n - 1; i >= 0; i--){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    arr[2] = 60;
+    n++;
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
